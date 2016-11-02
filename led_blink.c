b0e1044e4fa4a3ae6301ed6b0ab77fffcb6acbcf
Tiryoh <tiryoh@gmail.com>
2016-11-02 12:11:52 +0900

    Add func to get chrdev_region

--- led_blink.c
+++ led_blink.c
@@ -1,17 +1,27 @@
 #include <linux/module.h>
+#include <linux/fs.h>
 
 MODULE_AUTHOR("Tiryoh");
 MODULE_DESCRIPTION("A simple driver for controlling RasPi GPIO");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
+static dev_t dev;
+
 static int __init init_mod(void){
-    printk(KERN_INFO "%s is loaded.\n", __FILE__);
+    int retval;
+    retval = alloc_chrdev_region(&dev, 0, 1, "led_blink");
+    if(retval < 0){
+        printk(KERN_ERR "alloc_chrdev_region failed\n");
+        return retval;
+    }
+    printk(KERN_INFO "%s is loaded. major:%d\n", __FILE__, MAJOR(dev));
     return 0;
 }
 
 static void __exit cleanup_mod(void){
-    printk(KERN_INFO "%s is unloaded.\n", __FILE__);
+    unregister_chrdev_region(dev, 1);
+    printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
 }
 
 module_init(init_mod);
