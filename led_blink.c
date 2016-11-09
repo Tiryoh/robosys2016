54981cc5ebd31a09ffa4982f49e659e2d8e9352e
Tiryoh <tiryoh@gmail.com>
2016-11-09 12:18:38 +0900

    Add "SUSHI"

--- led_blink.c
+++ led_blink.c
@@ -21,9 +21,22 @@ static ssize_t led_write(struct file* flip, const char* buf, size_t count, loff_
     return 1;
 }
 
+static ssize_t sushi_read(struct file* flip, const char* buf, size_t count, loff_t* pos){
+    int size = 0;
+    char sushi[] = {0xF0, 0x9F, 0x8D, 0xA3, 0x0A}; //sushi
+    if(copy_to_user(buf+size, (const char *)sushi, sizeof(sushi))){
+        printk(KERN_INFO "sushi : copy_to_user failed\n");
+        return -EFAULT;
+    }
+    size += sizeof(sushi);
+    return size;
+}
+
+
 static struct file_operations led_fops = {
     .owner = THIS_MODULE,
-    .write = led_write
+    .write = led_write,
+    .read = sushi_read
 };
 
 static int __init init_mod(void){
