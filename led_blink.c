4cbbf3a6a9733bca7b54b13d08f50182efaa42d9
Tiryoh <tiryoh@gmail.com>
2016-11-09 11:12:42 +0900

    Add character device

--- led_blink.c
+++ led_blink.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 MODULE_AUTHOR("Tiryoh");
 MODULE_DESCRIPTION("A simple driver for controlling RasPi GPIO");
@@ -7,6 +8,17 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
 static dev_t dev;
+static struct cdev cdv;
+
+static ssize_t led_write(struct file* flip, const char* buf, size_t count, loff_t* pos){
+    printk(KERN_INFO "led_write  is called\n");
+    return 1;
+}
+
+static struct file_operations led_fops = {
+    .owner = THIS_MODULE,
+    .write = led_write
+};
 
 static int __init init_mod(void){
     int retval;
@@ -16,10 +28,19 @@ static int __init init_mod(void){
         return retval;
     }
     printk(KERN_INFO "%s is loaded. major:%d\n", __FILE__, MAJOR(dev));
+
+    cdev_init(&cdv, &led_fops);
+    retval = cdev_add(&cdv, dev, 1);
+    if(retval < 0){
+        printk(KERN_ERR "cdev_add failed. major:%d, minor:%d", MAJOR(dev), MINOR(dev));
+        return retval;
+    }
+
     return 0;
 }
 
 static void __exit cleanup_mod(void){
+    cdev_del(&cdv);
     unregister_chrdev_region(dev, 1);
     printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
 }
